3993747ad4eaa5f1abd29583ceba671e56a3f13f
Nathan Walston <nathanielwalston@gmail.com>
2021-11-27 10:48:39 -0800

    Respond with HTML for consistency sake

--- 02/server.js
+++ 02/server.js
@@ -1,8 +1,16 @@
 const polka = require("polka");
 const app = polka();
 
+const HTML = `
+<html>
+	<body>
+		<p>Hello, World!</p>
+	</body>
+</html>
+`;
+
 app.get("/", function (req, res) {
-  res.end("Hello, World!");
+  res.end(HTML);
 });
 
 app.listen(8080, function mountNotification() {
