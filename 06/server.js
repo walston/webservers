2e4f6dd14bd232e3282a7f1d19704010f97224ee
Nathan Walston <nathanielwalston@gmail.com>
2021-11-28 11:46:06 -0800

    Add code comments to exercise 6

--- 06/server.js
+++ 06/server.js
@@ -1,11 +1,23 @@
 const polka = require("polka");
+/** @NOTE Some packages include several exports at the top level, like this. */
 const textBodyParser = require("body-parser").text();
 
 /////////////// GLOBALS ///////////////
 const app = polka();
+/**
+ * @NOTE
+ * this is considered an "in-memory" dataset, definitely not scalable but
+ * it works for this example case */
 const USERS = ["Huey", "Dewey", "Louis"];
 
 /////////////// APP LOGIC ///////////////
+/**
+ * @NOTE
+ * middleware **cascades**, meaning the functions are all executed in order,
+ * and the same request object is passed to each.
+ * If we were to put our logger middleware before textBodyParser `req.body`
+ * would be undefined as it is by default. textBodyParser adds `req.body` to
+ * the `req` object and would not exist otherwise. */
 app.use(textBodyParser, function (req, res, next) {
   console.log(req.method, req.url, req.body);
   next();
