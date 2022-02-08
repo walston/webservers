dbb3212fbe9c543aa2c99e3d1940197103113ad1
Nathan Walston <nathanielwalston@gmail.com>
2022-02-07 20:52:45 -0800

    Adds a success page

--- final/server/index.js
+++ final/server/index.js
@@ -1,6 +1,15 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+
+const users = new Map();
+[
+  { id: "john@beatles.com", password: "Password123" },
+  { id: "paul@beatles.com", password: "Password123" },
+  { id: "george@beatles.com", password: "Password123" },
+  { id: "ringo@beatles.com", password: "Password123" },
+].forEach((user) => users.set(user.id, user));
 
 app.use(express.static("./public"));
 app.use(express.json());
@@ -10,6 +19,28 @@ app.use(function incoming(req, _res, next) {
   console.log(req.body);
   next();
 });
+
+app.post(
+  "/submit",
+  bodyParser.urlencoded({ type: "application/x-www-form-urlencoded" }),
+  function handle(req, res) {
+    const storedUser = users.get(req.body.username);
+    const storedPassword = storedUser?.password;
+    const inputPassword = req.body.password;
+
+    if (storedUser && storedPassword === inputPassword) {
+      console.log("logging in as", storedUser.id);
+      res.setHeader("Location", "/success.html?user=" + storedUser.id);
+      res.statusCode = 301;
+      res.send();
+    } else {
+      console.log("Unable to log in as", storedUser?.id);
+      res.setHeader("Location", "/?error=Unable to login");
+      res.statusCode = 301;
+      res.send();
+    }
+  }
+);
 
 app.use(function response(req, res, next) {
   console.log(res.statusCode);
